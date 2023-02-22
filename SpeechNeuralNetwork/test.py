48918a8256eeabe1f65b068511c26feb91790125
blackflame576 <pws15548@gmail.com>
2023-02-22 21:34:57 +0300

    Deploy

---
+++ SpeechNeuralNetwork/test.py
@@ -0,0 +1,10 @@
+# import pandas as pd
+# nums = []
+# names = []
+# df = pd.read_csv('Datasets/TTSMetadata.csv',sep = '\t')
+# for column in df["000000_RUSLAN|С тревожным чувством берусь я за перо."]:
+#     arr = column.split("|")
+#     nums.append(arr[0])
+#     names.append(arr[1])
+# print(nums[:10])
+# # print(names)
